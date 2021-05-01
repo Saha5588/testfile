81480fd77983eb50d4c9690350ac5916a9a826fe
Saha5588 <mgacode3@gmail.com>
2021-05-01 21:58:34 +0530

    New Project

---
+++ test.py
@@ -0,0 +1,13 @@
+name = input('what is your name ?')
+print('your name is ' + name)
+color = input('what is your favourite color?')
+print( name + ' likes ' + color)
+my_name = "Madhurjya Saha"
+print(len(my_name))
+print(my_name[9:-1])
+print(my_name.upper())
+print(my_name.replace("M" , "S"))
+print(my_name.title())
+print(my_name.find("s"))
+print(bool(""))
+print('hello world')
